d07f2fbcac176bcce429e32d68c02c3d376d77af
zxd <zzuzxd@126.com>
2017-12-26 20:21:12 +0800

    加入mpl不同方式

--- 5.移动坐标轴.py
+++ 5.移动坐标轴.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mlp
+mlp.style.use('seaborn-ticks')#选择不同的显示方式
+print(mlp.style.available)
 plt.rcParams['font.sans-serif'] = ['SimHei']  #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  #用来正常显示负号
 
