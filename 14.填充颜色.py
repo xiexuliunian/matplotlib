6b65434c06ff380e940285df9491714030b10e2f
zxd <zzuzxd@126.com>
2017-12-29 19:28:46 +0800

    规范写法

--- 14.填充颜色.py
+++ 14.填充颜色.py
@@ -13,8 +13,8 @@ plt.plot(x,s,color='r',label='sin')
 plt.plot(x,c,color='g',label='cos')
 # plt.fill_between(x,0,s,color='C3')#显示sin曲线和y=0围成的面积
 #fill_between(x, y1, y2=0, where=None, interpolate=False)
-plt.fill_between(x,c,s,where=c>=s,interpolate=True,color='C1',alpha=0.5)
-plt.fill_between(x,c,s,where=c<=s,interpolate=True,color='C2',alpha=0.5)
+plt.fill_between(x,c,s,where=(c>=s),interpolate=True,color='C1',alpha=0.5)
+plt.fill_between(x,c,s,where=(c<=s),interpolate=True,color='C2',alpha=0.5)
 plt.fill_between(x[20:43],c[20:43],s[20:43],color='C6')
 
 plt.legend()
